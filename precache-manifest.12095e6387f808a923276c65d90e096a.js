6a01434c1494c1d0d9cdf0649a02491e08d32003
gitAlbertadine <albertadine760@gmail.com>
2019-04-25 21:41:14 +0100

    Updates

---
+++ precache-manifest.12095e6387f808a923276c65d90e096a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a40cef86630103e9cbac3d0c2db3677",
+    "url": "/reacttest/index.html"
+  },
+  {
+    "revision": "c0c3314f0466cf058cab",
+    "url": "/reacttest/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/reacttest/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c0c3314f0466cf058cab",
+    "url": "/reacttest/static/js/main.ac1bee71.chunk.js"
+  },
+  {
+    "revision": "db7600c47e6e7907c405",
+    "url": "/reacttest/static/js/runtime~main.492d25e7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacttest/static/media/logo.5d5d9eef.svg"
+  }
+]);
